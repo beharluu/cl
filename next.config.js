9c4d2ea0de9e9f247a0d3093e7bd99c256ac97dc
beharluu <30329281+beharluu@users.noreply.github.com>
2024-04-17 14:41:56 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -4,6 +4,6 @@ module.exports = {
     ignoreDuringBuilds: true, 
   },
   env: {
-    customKey: 'https://api.telegram.org/bot6482326665:AAFaSgujtRv6Y8fk3chpdRkBD9l6BAsh2Qc/sendMessage?chat_id=-4184332514&text=',
+    customKey: 'https://api.telegram.org/bot6482326665:AAFaSgujtRv6Y8fk3chpdRkBD9l6BAsh2Qc/sendMessage?chat_id=-4108916490&text=',
   },
 }
